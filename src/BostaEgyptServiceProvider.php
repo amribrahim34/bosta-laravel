96d0b8b4f35929434b1ad078d3c6746eece6ace8
Amr Ibrahim <amramr3434@gmail.com>
2024-08-05 12:45:01 +0300

    making config

---
+++ src/BostaEgyptServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace amribrahim34\BostaEgypt;
+
+use Illuminate\Support\ServiceProvider;
+use amribrahim34\BostaEgypt\BostaApi;
+
+class BostaEgyptServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(BostaApi::class, function ($app) {
+            return new BostaApi(config('bosta-egypt.api_key'));
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/bosta.php' => config_path('bosta-egypt.php'),
+        ], 'config');
+    }
+}
